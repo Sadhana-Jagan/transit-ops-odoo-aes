36a970c1665661e4328787885b0347d95235c683
Elakya <115806138+elakya04@users.noreply.github.com>
2026-07-12 14:52:26 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'hhttps://transit-ops-odoo-aes.onrender.com',
         changeOrigin: true,
       },
     },
