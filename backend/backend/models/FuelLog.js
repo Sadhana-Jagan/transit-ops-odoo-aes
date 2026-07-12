3d45f2f64dcc8f20640691bd0293ead4d3468569
Aravind <aravindfungame@gmail.com>
2026-07-12 10:46:37 +0530

    Backend skeleton

--- backend/backend/models/FuelLog.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose");
-
-module.exports=mongoose.model("FuelLog",new mongoose.Schema({
-    vehicle:{type:mongoose.Schema.Types.ObjectId,ref:"Vehicle",required:true},
-    trip:{type:mongoose.Schema.Types.ObjectId,ref:"Trip"},
-    liters:{type:Number,required:true},
-    cost:{type:Number,required:true},
-    date:{type:Date,default:Date.now}
-},{timestamps:true}));
